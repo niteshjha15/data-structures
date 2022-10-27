cdbc7b2eb205b9f3837afd47283ef6dbed71b3fb
niteshjha15 <66312321+niteshjha15@users.noreply.github.com>
2022-10-27 10:55:01 +0530

    Graph implimentation

--- index.js
+++ index.js
@@ -1,165 +1,69 @@
-// This class instance creats a new head opject with value and next e.g. {node:10,
-//next==null}
-class Node {
-  constructor(value) {
-    this.node = value;
-    this.next = null;
-  }
-}
+// This is the implimentaion of basic graph data structure
 
-class LinkedList {
+// Refer to the diagram(Tree.drawio) to visualize the tree
+
+// There are two types of graph representation
+// 1.) Adjacent Matrix : Represents graph vertexs and edges as a 2D array refer to Tree.drawio
+// 2.) Adjacent List: Represents graps as a object or dictionary where key is the vertex and value is a list of
+//     connection with vertex.
+
+// An Adjacent list implimentation:
+class Graph {
   constructor() {
-    this.size = 0;
-    this.head = null;
+    this.list = {};
   }
 
-  isEmpty() {
-    return this.size === 0 ? true : false;
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  append(value) {
-    let newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.head = newNode;
-    }else{
-        let curr = this.head;
-        while (curr.next) {
-          curr = curr.next;
-        }
-        curr.next = newNode;
-        
-    }
-    this.size++;
-    
-  }
-
-  prepend(value) {
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.head = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    this.size++;
-  }
-
-  insert(value, index) {
-    if (index < 0) {
-      return;
-    }
-    if (index === 0) {
-      this.prepend(value);
-    } else if (index > 0 && this.size > index) {
-      let newNode = new Node(value);
-      let curr = this.head;
-      for (let i = 0; i < index - 1; i++) {
-        curr = curr.next;
-      }
-      newNode.next = curr.next;
-      curr.next = newNode;
-      this.size++;
+  addVertex(vertex) {
+    if (!this.list[vertex]) {
+      this.list[vertex] = new Set();
     }
   }
 
-  removeFrom(index) {
-    let removedNode;
-    if (index >= this.size || index < 0) {
-      return null;
-    } else if (index === 0) {
-      let refrenceNode;
-      removedNode = this.head;
-      refrenceNode = this.head.next;
-      this.head = refrenceNode;
-      this.size--;
-      return removedNode.node;
-    } else {
-      let removedNode;
-      let refrenceNode;
-      let curr = this.head;
-      for (let i = 0; i < index - 1; i++) {
-        curr = curr.next;
-      }
-      removedNode = curr.next;
-      refrenceNode = removedNode.next;
-      curr.next = refrenceNode;
-      this.size--;
-      return removedNode.node;
+  addEdge(vertex1, vertex2) {
+    if (!this.list[vertex1]) {
+      this.addVertex(vertex1);
     }
+    if (!this.list[vertex2]) {
+      this.addVertex(vertex2);
+    }
+    this.list[vertex1].add(vertex2);
+    this.list[vertex2].add(vertex1);
   }
 
-  removeByValue(value){
-    if(value===this.head.node){
-      let temp = this.head.node
-      this.head = this.head.next;
-      return temp
-    }
-    let curr = this.head;
-    while(curr.next && curr.next.node !==value){
-       curr = curr.next
-    }
-    let removedNode = curr.next;
-    if(removedNode){
-      curr.next = removedNode.next;
-      return removedNode.node
-    }
-    return null
-   
+  pathExists(vertex1, vertex2) {
+    if (!this.list[vertex1]) return false;
+    if (!this.list[vertex2]) return false;
+    return this.list[vertex1].has(vertex2) && this.list[vertex2].has(vertex1);
   }
 
-  searchValue(value){
-    if(this.isEmpty()){
-      return null
-    }
-    let i = 0;
-    let curr = this.head;
-    while(curr){
-      if(curr.node===value){
-        return i
-      }
-       curr = curr.next;
-       i++
-    }
-    return -1;
+  removeEdges(vertex1, vertex2) {
+    if (!this.list[vertex1]) return;
+    if (!this.list[vertex2]) return;
+    this.list[vertex1].delete(vertex2);
+    this.list[vertex2].delete(vertex1);
   }
 
-  reverseList(){
-    let curr = this.head;
-    let prev = null;
-
-    while(curr){
-      let next = curr.next;
-      curr.next = prev;
-      prev=curr;
-      curr = next;    
+  removeVertex(vertex){
+    if (!this.list[vertex]) return;
+    for(let item in this.list){
+      this.list[item].delete(vertex)
     }
-    this.head = prev;
+    delete this.list[vertex]
   }
 
-  print() {
-    let curr = this.head;
-    let values = "";
-    while (curr) {
-      values += ` ${curr.node}`;
-      curr = curr.next;
+  display() {
+    for (let item in this.list) {
+      console.log(item + "->" + [...this.list[item]]);
     }
-    return values;
   }
 }
 
-const list = new LinkedList();
-list.append(10);
-list.append(20);
-list.append(30);
-list.prepend(5)
-list.append(40)
-list.append(50)
-list.append(60)
-list.append(70)
-console.log("list", list.print());
-list.reverseList()
-console.log("list", list.print());
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+graph.removeVertex("C")
+graph.display();
